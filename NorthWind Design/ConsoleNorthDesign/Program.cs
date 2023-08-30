203c6cd36ba102b6632bc8c909868c9c9ce05afb
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 17:02:59 +0300

    NorthWind tables in C# design

---
+++ NorthWind Design/ConsoleNorthDesign/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNorthDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new NorthwindEntities();
+
+            foreach (var customer in data.Customers)
+            {
+                Console.WriteLine(customer.ContactName);
+            }
+        }
+    }
+}
